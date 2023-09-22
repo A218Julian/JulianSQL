58968126e66d2a6a02233e29309c1560acf97e6a
Julian <145588903+A218Julian@users.noreply.github.com>
2023-09-22 11:14:27 -0400

    Update OwnerRevCompl.sql

--- OwnerRevCompl.sql
+++ OwnerRevCompl.sql
@@ -33,32 +33,10 @@ FROM
     Contact C WITH (NOLOCK)
 CROSS APPLY #DateRange
 WHERE
-    ContactID IN (
-    4556593 --Mike Martin
-   ,88442321 --Liz Mitchell
-   ,6334296 --James Regoord
-   ,117653641 --Robert Schwartz
-   ,118443721 --Laisha Peters
-   ,154208081 --Jacob Floyd 
+    ContactID IN (---
+  
    ,74683051 --Julian Sebia
-   ,44387711 --Matthew McClenahan
-   ,4204161 --Ernest Rodriguez
-   ,61351271 --Eric Byerley
-   ,4779152 --Shavon Shockley
-   ,3978801 --Victoria Cottman
-   ,43784101 --Sean-Evan Evaro
-   ,84588351 --Clenda Membreno
-   ,4778811 --Greg Charlton
-   ,4579917 --Bryan McCaslin
-   ,107025241 --Thomas Meldrum
-   ,91341081 --Justin Diggs
-   ,4426032 --Fran Koerner
-   ,45024231 --Anne Scobell
-   ,43486541 --Scott Cooper
-   ,5102581 --Scott Layton
-   ,63911361 --Madison Palmer
-   ,6781531 --Nicklaus Smith
-   ,98945391) --Molly Barcikowski
+---
 
 IF OBJECT_ID('tempdb.dbo.#ContactActivities') IS NOT NULL
 DROP TABLE #ContactActivities
